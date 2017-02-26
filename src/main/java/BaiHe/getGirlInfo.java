a4d1f343559b7424124022525651e25b9446df83
caogen <774750747@qq.com>
2017-02-26 22:09:16 +0800

    爬虫百合网(按颜值过滤)

---
+++ src/main/java/BaiHe/getGirlInfo.java
@@ -0,0 +1,67 @@
+package BaiHe;
+
+import java.util.ArrayList;
+import javax.management.JMException;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.monitor.SpiderMonitor;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class getGirlInfo implements PageProcessor {
+
+	private Site site = Site.me().setRetryTimes(3).setSleepTime(1000)
+			.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36");
+
+    public void process(Page page) {
+    	page.putField("url", page.getHtml().xpath("//div[@class='pic']/a/img/@src").all());
+    	
+    	ArrayList<String> list = page.getResultItems().get("url");
+    	
+    	for(int i = 0;i < list.size(); i++){
+    		String url = list.get(i); 
+    		double value = FaceTest.getInfo(url);
+    		if(value>80){
+    			String imgFileName = url.substring(url.lastIndexOf("/")+1);
+    			
+    			String[] fileName = imgFileName.split("\\.");
+    			
+    			StringBuffer str = new StringBuffer(fileName[0]+"("+value+")");
+    			str.append("."+fileName[1]);
+            	
+            	byte[] btImg = GetImage.getImageFromNetByUrl(url);  
+                if(null != btImg && btImg.length > 0){  
+                    System.out.println("读取到：" + btImg.length + " 字节");  
+                    GetImage.writeImageToDisk(btImg, str.toString());  
+                }else{  
+                    System.out.println("没有从该连接获得内容");  
+                }  
+    		}
+        	
+    	}
+    	
+
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) throws JMException {
+    	
+    	Spider testSpider = Spider.create(new BaiHeUserPageProcessor());
+    	
+    	testSpider.addUrl("http://search.baihe.com/solrAdvanceSearch.action");
+    	testSpider.addPipeline(new ConsolePipeline());
+        testSpider.thread(5);
+        
+        //注册监控
+        SpiderMonitor.instance().register(testSpider);
+        
+        testSpider.start();
+        
+    }
+
+}
